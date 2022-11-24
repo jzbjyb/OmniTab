1fbfb06d78204a936ddcab4584de24a2bd74e625
Zhengbao Jiang <zhengbao@learnfair0746.h2.fair>
2022-11-24 11:16:12 -0800

    added the latest huggingface script to run TAPEX/OmniTab

---
+++ test_wikitablequestions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --time=8:00:00
+#SBATCH --partition=learnlab
+#SBATCH --constraint=volta32gb
+#SBATCH -o slurm/%x.%j.out
+#SBATCH -e slurm/%x.%j.err
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=8
+#SBATCH --cpus-per-task=80
+#SBATCH --mem=256GB
+#SBATCH --job-name=wtq
+
+deepspeed run_wikitablequestions.py \
+  --do_predict \
+  --model_name_or_path neulab/omnitab-large-finetuned-wtq \
+  --output_dir test \
+  --per_device_eval_batch_size 6 \
+  --predict_with_generate \
+  --num_beams 5
